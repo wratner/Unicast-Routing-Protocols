b28a45428feef28f5128bf57cad0b57598a80021
smtanaka12 <smtanaka12@gmail.com>
2014-03-13 21:32:08 -0500

    Update manager.c

--- manager.c
+++ manager.c
@@ -19,7 +19,7 @@ void *get_in_addr(struct sockaddr *sa)
 
 int main(int argc, char *argv[])
 {
-	
+
 	if (argc != 3) {
 		fprintf(stderr,"usage: manager topologyfile messagefile\n");
 		exit(1);
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
 	for (i = 0; i < totalMessages; i++) {
 		printf("%d %d %s\n",srcNode[i], destNode[i], message[i]);
 	}
-	
+
 	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
 	struct addrinfo hints, *servinfo, *p;
 	struct sockaddr_storage their_addr; // connector's address information
@@ -45,8 +45,9 @@ int main(int argc, char *argv[])
 	int yes = 1;
 	char s[INET6_ADDRSTRLEN];
 	int rv;
-	int numbytes;
-	char buf[MAXDATASIZE];
+	int dead = 0;
+	char * welcome;
+	welcome = "Hello!";
 
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
@@ -57,38 +58,38 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "manager: getaddrinfo error: %s\n", gai_strerror(rv));
 		return 1;
 	}	
-	
+
 	// loop through all the results and bind to the first we can
 	for(p = servinfo; p != NULL; p = p->ai_next) {
 		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
+			p->ai_protocol)) == -1) {
 			perror("manager: socket");
-			continue;
-		}
-	
-		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-				sizeof(int)) == -1) {
-			perror("manager: setsockopt");
-			exit(1);
-		}
-
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("manager: bind");
-			continue;
-		}
-		break;
+		continue;
 	}
+        //Set socket options
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+		sizeof(int)) == -1) {
+		perror("setsockopt");
+	exit(1);
+}
 
-	if (p == NULL)  {
-		fprintf(stderr, "manager: failed to bind\n");
-		return 2;
-	}
+if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+	close(sockfd);
+	perror("manager: bind");
+	continue;
+}
+
+break;	}
+
+if (p == NULL)  {
+	fprintf(stderr, "manager: failed to bind\n");
+	return 2;
+}
 
 	freeaddrinfo(servinfo); // all done with this structure
 
 	if (listen(sockfd, BACKLOG) == -1) {
-		perror("manager: listen");
+		perror("listen");
 		exit(1);
 	}
 
@@ -96,55 +97,51 @@ int main(int argc, char *argv[])
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
 	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-		perror("manager: sigaction");
+		perror("sigaction");
 		exit(1);
 	}
 
-	printf("manager: waiting for connections...\n");
-	
-	//spawn a new process for each router
-	//int i = 0;
-	for(i = 0; i < nNodes; i++) {
-		if (!fork()) 
-			{ // this is the child router process
-				close(sockfd);
-				//executeRouter(argv[1]);
-			}
-	}
 
-	int sockArray[nNodes];
-	for (i = 0; i < nNodes; i++){
+	printf("manager: waiting for connections...\n");
+
+
+	while(1) {
+
 		sin_size = sizeof their_addr;
 		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
 		if (new_fd == -1) {
-			perror("manager: accept");
+			perror("accept");
 			continue;
-		}
-		else {
-			sockArray[i] = new_fd;
 		}
 
 		inet_ntop(their_addr.ss_family,
 			get_in_addr((struct sockaddr *)&their_addr),
 			s, sizeof s);
-		//printf("manager: got connection from %s\n", s);
+		printf("server: got connection from %s\n", s);
+		if (!fork()) {
+			close(sockfd);
 
-		if ((numbytes = recv(new_fd, buf, MAXDATASIZE-1, 0)) == -1) {
-			perror("router: recv");
-			exit(1);
+		if(send(new_fd, welcome, sizeof(welcome), 0) == -1){
+			perror("send");
+			exit(0);
 		}
-		buf[numbytes] = '\0';
-		//assignPorts(topology, i, atoi(buf));
-	}
-	
-	
-	for (i = 0; i < nNodes; i++){
-		new_fd = sockArray[i];
-		if (send(new_fd, (&topology[i]), sizeof(node), 0) == -1)
-			perror("manager: send");
 
-		close(new_fd); 
-	}
-	
-	return 0;
+		if(send(new_fd, topology, sizeof(topology), 0) == -1){
+			perror("send");
+			exit(0);
+		}
+
+		//while(dead == 0) {
+
+
+
+
+		//} 
+
+
+			close(new_fd);
+		} 
+
+		return 0;
+}
 }
