836dcdaad0ce2b22233a05620373d381d084b512
smtanaka12 <smtanaka12@gmail.com>
2014-03-23 13:41:18 -0500

    Update distvec.c

--- distvec.c
+++ distvec.c
@@ -1,11 +1,118 @@
+/*
+** client.c -- a stream socket client demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include "mp2.h"
+#include <arpa/inet.h>
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char *argv[])
+{
+	int sockfd, numbytes;  
+	char buf[MAXDATASIZE];
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	char s[INET6_ADDRSTRLEN];
+    char * port;
+    node * in_node = NULL;
+
+    //Check for the correct number of args
+	if (argc != 2) {
+	    fprintf(stderr,"usage: client hostname \n");
+	    exit(1);
+	}
+
+
+
+    port = "2345";
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ((rv = getaddrinfo(argv[1], port, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and connect to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("client: socket");
+			continue;
+		}
+
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("client: connect");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "client: failed to connect\n");
+		return 2;
+	}
+
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+			s, sizeof s);
+	printf("client: connecting to %s\n", s);
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+
+//Setup complete
+
+    //while(1){
+
+   	    if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+	        perror("recv");
+            exit(1);
+	    } 
+
+	    buf[numbytes] = '\0';
+
+        //Print message from the server
         printf("%s\n", buf);
 
-        //Client guessed the correct numbers, close connection
-        if(strcmp(buf, "Correct!") == 0){
-            break;
+        if((numbytes = recv(sockfd, in_node, MAXDATASIZE-1, 0)) == -1){
+        	perror("recv");
+            exit(1);
         }
 
-        //Client used 8 guesses, close connection
-        if(strcmp(buf, "Game over") == 0){
-            break;
-        }
+        print_node(in_node, 2);
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
