e763d431afb4e1693081ffd09e4998bb4857bf4a
smtanaka12 <smtanaka12@gmail.com>
2014-03-23 12:54:07 -0500

    Connects and receives message from manager.
      Then "bad file descriptor message"

--- distvec.c
+++ distvec.c
@@ -1,56 +1,11 @@
-/** Distance Vector Protocol **/
-/*
-** distvec.c 
-*/
+        printf("%s\n", buf);
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <sys/wait.h>
-#include <signal.h>
+        //Client guessed the correct numbers, close connection
+        if(strcmp(buf, "Correct!") == 0){
+            break;
+        }
 
-#define BACKLOG 10	 // how many pending connections queue will hold
-
-/*
- * 
- */
-
-
-
-/*
- * printForwardingTable
- * Prints the fowrarding table for the specified nodes
- * Input: Table
- * Output: Forwarding table for current node in the format:
-           destination nexthop pathcost
- */
-void printForwardingTable(distvec_rt * table){
-
-    int a = 0;
-    for (a = 0; a < table -> numNodes; a++){
-        printf("%d %d %d \n", table -> destination[a], table -> nexthop[a], table -> pathcost[a]);     
-    }
-}
-
-dvRoutingTable * initDVRT(struct node * connectTable){
-	dvRoutingTable * table = (dvRoutingTable *) malloc(connectTable-> curr_index) * sizeof(dvRoutingTable));
-	table -> nodeID = connectTable -> node_num;
-	table -> numNodes = connectTable -> arr_length;
-	table -> numNeighbors = connectTable -> curr_index;
-
-	table -> nextHop = malloc((connectTable->arr_length) * sizeof(int));
-	table -> linkCost = mallow((connectTable -> arr_length) * sizeof(int));
-
-	int
-}
-
-}
-
+        //Client used 8 guesses, close connection
+        if(strcmp(buf, "Game over") == 0){
+            break;
+        }
